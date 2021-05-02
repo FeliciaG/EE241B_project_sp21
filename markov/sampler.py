bade82104b6612f337737f92760fd280a850edb3
Class Account <ee241-aai@eda-3.EECS.Berkeley.EDU>
2021-05-02 01:01:31 -0700

    basic sampling script for strongarm

---
+++ markov/sampler.py
@@ -0,0 +1,31 @@
+import csv
+
+clk_period = 4e-6;
+delay = 2100e-9;
+
+fileo = open(r"outputs.txt", "w+")
+time = delay
+row_write = 0
+linewrite = 0
+with open('outputs/out_digital3.csv') as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter=',')
+	
+	line_count = 0
+	for row in csv_reader:
+		if line_count == 0:
+			line_count += 1
+		else:
+			while(time < float(row[0])):
+				fileo.write(str(row_write))
+				linewrite += 1
+				time = time + clk_period
+				if (linewrite == 24):
+					fileo.write("\n")
+					linewrite = 0
+			row_write = int(row[1])
+			line_count += 1
+			
+fileo.close()
+			
+	
+			
