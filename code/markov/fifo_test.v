8015f918c3b471fd4f8f9ed72baeb40aada8cce2
Class Account <ee241-aai@eda-3.EECS.Berkeley.EDU>
2021-05-04 20:34:35 -0700

    output buffer written

--- code/markov/fifo_test.v
+++ code/markov/fifo_test.v
@@ -3,64 +3,74 @@
 module mc_tester();
     reg clk;
     reg reset;
-    reg in;
+    reg [5:0] valid;
+    reg [5:0] bits;
 
-    wire [3:0] lane;
-    wire out;
+    wire out_valid;
+    wire [15:0] out;
     initial clk = 0;
     initial reset =0;
     always #(10) clk <= ~clk;
+    wire [5:0] writing;
 
-    markov16 dut (.clk(clk), .reset(reset), .bit_in(in),
-    .lane(lane), .bit_out(out)
+    fifo16 dut (.clk(clk), .reset(reset), .valid(valid),
+    .bits(bits), .out_valid(out_valid), .out_16(out), .writing(writing)
     );
 
     initial begin
-        $dumpfile("mc_test.vpd");
+        $dumpfile("fifo_test.vcd");
         $dumpvars(0, dut);
         $display("start sim");
         @(posedge clk);
         reset = 1;
-        @(posedge clk);
-        reset = 0;
-
+		#1;
         @(posedge clk) begin
-            in = 0;
+			reset = 0;
+            bits = 6'b0;
+            valid = 6'b0;
         end
         #1;
-        $display("in: %b, lane: %b, out: %b", in, lane, out);
+        $display("in: %b, out_valid: %b, out: %b", bits, out_valid, out);
 
         @(posedge clk) begin
-            in = 0;
+            bits =  6'b111111;
+            valid = 6'b011111;
         end
         #1;
-        $display("in: %b, lane: %b, out: %b", in, lane, out);
+        $display("in: %b, out_valid: %b, out: %b, writing: %b", bits, out_valid, out, writing);
 
         @(posedge clk) begin
-            in = 1;
+            bits =  6'b010001;
+            valid = 6'b100111;
         end
         #1;
-        $display("in: %b, lane: %b, out: %b", in, lane, out);
+        $display("in: %b, out_valid: %b, out: %b", bits, out_valid, out);
 
         @(posedge clk) begin
-            in = 1;
+            bits =  6'b010101;
+            valid = 6'b110111;
         end
         #1;
-        $display("in: %b, lane: %b, out: %b", in, lane, out);
-
+        $display("in: %b, out_valid: %b, out: %b", bits, out_valid, out);
+		
+		@(posedge clk) begin
+            bits =  6'b110101;
+            valid = 6'b110111;
+        end
+        #1;
+        $display("in: %b, out_valid: %b, out: %b", bits, out_valid, out);
+        
         @(posedge clk) begin
-            in = 0;
+            bits =  6'b110101;
+            valid = 6'b100110;
         end
         #1;
-        $display("in: %b, lane: %b, out: %b", in, lane, out);
-
-        @(posedge clk) begin
-            in = 1;
-        end
-        #1;
-        $display("in: %b, lane: %b, out: %b", in, lane, out);
-
-        $finish();
+        $display("in: %b, out_valid: %b, out: %b", bits, out_valid, out);
+        #20;
+        $display("in: %b, out_valid: %b, out: %b", bits, out_valid, out);
+                #20;
+        $display("in: %b, out_valid: %b, out: %b", bits, out_valid, out);
+        #20 $finish();
     end
 
 endmodule
--- code/markov/markov.v
+++ code/markov/markov.v
@@ -42,27 +42,67 @@ module fifo16(
 	input [5:0] bits,
 	
 	output out_valid,
-	output [15:0] out_16
+	output [15:0] out_16,
+	output [5:0] writing
 );
-	reg [15:0] register;
-	reg [3:0] counter;
-	wire [3:0] counter_wire;
-		
-	integer i;
-	reg ones;
-	always @(posedge clk) begin
-		ones = 0;
-		for(i=0; i<6; i=i+1) begin
-			if (valid[i] & (counter+ones)<15) begin
-				register[counter+ones+1] <= bits[i];
-				ones = ones + 1;
-			end
+	reg [20:0] register;
+	reg [4:0] counter;
+	wire [4:0] counter_wire;
+	wire [4:0] counter_up;
+	reg out_valid_reg;
+	
+	//count number of valid bits	
+	reg [2:0] num_valid;
+	reg [2:0] num_valid_n;
+	reg [5:0] valid_bits;
+	reg [5:0] write_bits;
+	integer idx;
+	always @* begin
+		num_valid= {2{1'b0}};
+		valid_bits = {5{1'b0}};
+		for( idx = 0; idx<6; idx=idx+1) begin
+			num_valid = num_valid + valid[idx];
+			valid_bits[num_valid-1] = valid[idx] ?  bits[idx] : valid_bits[num_valid-1];
 		end
-		counter <= counter_wire;
+		num_valid_n = 6-num_valid;
+		write_bits = {{num_valid_n{1'b0}}, valid_bits}; 
 	end
-	assign counter_wire = (counter == 15) ? 0 : (counter + ones);
-	assign out_16 = register;
-	assign out_valid = (counter == 15);
+	assign  writing = write_bits;
+	
+	integer i;
+	integer j;
+	reg out_16_reg;
+	always @(posedge clk) begin
+		if (reset == 1) begin
+			counter<=0;
+			register <= 16'b0;
+			out_valid_reg <= 0;
+		end
+		else if (counter_wire >= 15) begin
+			counter <= (|valid) ? (counter_wire + num_valid -15): (counter_wire - 15);
+			for (i=15; i<20; i=i+1) begin
+				register[i-15] <= register[i+1];
+			end
+			for (j=0; j<num_valid; j=j+1) begin
+				register[counter_wire - 15 + j] <= write_bits[j];
+			end
+			for (j = counter_wire + num_valid -15; j<21; j=j+1) begin
+				register[j] <= 0;
+			end
+			out_valid_reg <= 0;
+		end		
+		else begin 		
+			for (j=0; j<num_valid; j=j+1) begin
+				register[counter_wire + j] <= write_bits[j];
+			end
+			counter <= (|valid) ? (counter_wire + num_valid) : counter;
+			out_valid_reg <= ((|valid) & (counter_wire + num_valid) > 15) ? 1 : 0;
+		end
+	end
+	assign counter_wire = reset? 0 :counter;
+	assign counter_up = reset? 0 :counter+5;
+	assign out_16 = reset ? 0 :register[15:0];
+	assign out_valid = reset ? 0 :out_valid_reg;
 
 endmodule
 
@@ -80,7 +120,7 @@ module lfsr16(
         if (reset) begin
             register <= in_bits;
         end else begin
-            register <= {register[15:1] };
+            register <= {register[15:1], xor_bit };
         end
     end
 endmodule
