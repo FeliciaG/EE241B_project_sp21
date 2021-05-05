4258357e21c5ba191add682543e01b0e531284bb
Jingyi <jingyixu@berkeley.edu>
2021-05-04 21:04:18 -0700

    bbs output generated

--- code/bbs/bbs.v
+++ code/bbs/bbs.v
@@ -1,29 +1,32 @@
-module bbs #(parameter W=16, m=253)(    
+module bbs_top #(parameter W=16, m=253)(    
     input clk,
     input reset,
     input [W-1:0] seed,
-
+    input reseed,
+    output out_bit,
+    output out_valid,
+    // for debugging
     output [W-1:0] out);
 
     wire gcd_rdy;
     wire res_valid;
     wire seed_valid;
-    wire mm_start;
-    wire mm_done;
     wire [W-1:0] mm_out;
     wire wen;
     wire store_seed;
-    reg [W-1:0] out;
+    reg [W-1:0] out_reg;
     reg [W-1:0] x_val;
 
+    assign out = out_reg;
+    assign out_bit = out_reg[0];
     always@(posedge clk) begin
         if (reset) begin
-            out <= 0;
+            out_reg <= 0;
         end else begin
             if (store_seed) begin
-                out <= seed;
+                out_reg <= seed;
             end else begin
-                out <= wen? mm_out : out;
+                out_reg <= wen? mm_out : out_reg;
             end
         end
     end
@@ -36,14 +39,13 @@ module bbs #(parameter W=16, m=253)(
     );
 
     montgomery_mult#(.W(W), .m(m)) mm(
-        .clk(clk), .reset(reset), .x_val(out), .start(mm_start),
-        .done(mm_done), .out(mm_out)
+        .clk(clk), .reset(reset), .x_val(out_reg), .out(mm_out)
     );
 
     bbs_ctrl ctrl(
-        .clk(clk), .reset(reset), .mm_done(mm_done),
+        .clk(clk), .reset(reset), .out_valid(out_valid), .reseed(reseed),
         .seed_val(seed_valid && res_valid), .gcd_rdy(gcd_rdy),
-        .wen(wen), .mm_start(mm_start), .store_seed(store_seed)
+        .wen(wen),  .store_seed(store_seed)
     );
 
 endmodule
@@ -51,20 +53,22 @@ endmodule
 module bbs_ctrl(
     input clk,
     input reset,
-    input mm_done,
     input seed_val,
     input gcd_rdy,
-
+    input reseed,
+    output out_valid,
     output reg wen,
-    output reg mm_start, 
     output reg store_seed
 );
 
 parameter IDLE = 2'd0; //00
 parameter SEED = 2'd1; //01
-parameter MM_EX = 2'd2; //10
-parameter MM_DONE = 2'd3; //11
+parameter MM = 2'd2;
+parameter RESEED = 2'd3;
+// parameter MM_EX = 2'd2; //10
+// parameter MM_DONE = 2'd3; //11
 
+assign out_valid = state == MM || state == RESEED;
 reg  [1:0] state;
 reg  [1:0] next_state;
 
@@ -77,39 +81,41 @@ always @(*) begin
             next_state = IDLE;
         end
         wen = 1'b0;
-        mm_start = 1'b0;
         store_seed = 1'b0;
     end
 
     SEED:begin
         if (seed_val) begin
-            next_state = MM_EX;
+            next_state = MM;
         end else begin
             next_state = SEED;
         end
         wen = 1'b0;
-        mm_start = 1'b0;
         store_seed = 1'b1;
     end
 
-    MM_EX:begin
-        mm_start = 1'b1;
+    MM:begin
+        wen = 1'b1;
         store_seed = 1'b0;
-        if (mm_done) begin
-            wen = 1'b1;
-            next_state = MM_DONE;
+        if (reset) begin
+            next_state = IDLE;
+        end else if (reseed) begin
+            next_state = RESEED;
         end else begin
-            wen = 1'b0;
-            next_state = MM_EX;
+            next_state = MM;
         end
     end
 
-    MM_DONE: begin
-        store_seed = 1'b0;
-        wen = 1'b0;
-        next_state = MM_EX;
+    RESEED:begin
+        if (seed_val) begin
+            next_state = MM;
+            store_seed = 1'b1;
+        end else begin
+            next_state = RESEED;
+            store_seed = 1'b0;
+        end
+        wen = 1'b1;
     end
-
     endcase
 end
 
@@ -126,20 +132,10 @@ module montgomery_mult #(parameter W=16, m=253)(
     input clk,
     input [W-1:0] x_val,
     input reset,
-    input start,
-    output done,
     output [W-1:0] out
 );
-    assign out = x_val * x_val % m;
-    reg fake_done;
-    always @(posedge clk) begin
-        if (reset) begin
-            fake_done = 1'b0;
-        end else begin
-            fake_done = ~fake_done;
-        end
-    end
-    assign done = fake_done;
+    wire [31:0] extended_x = {16'b0, x_val};
+    assign out = (extended_x * extended_x) % m;
 
 endmodule
 
--- code/bbs/bbs_behavioral.cpp
+++ code/bbs/bbs_behavioral.cpp
@@ -20,39 +20,41 @@ class BBS{
             p = p0;
             q = q0;
             M = p0 * q0;
-            rand = seed;
+            my_rand = seed;
         }
         int set_seed(long seed) {
             if (gcd(M, seed) != 1) {
                 return 0;
             } else {
-               rand = seed; 
+               my_rand = seed; 
                return 1;
             }
         }
         long get_rand() {
-            long next_rand = rand * rand % M;
-            rand = next_rand;
-            return rand;
+            long next_rand = (my_rand * my_rand) % M;
+            my_rand = next_rand;
+            return my_rand;
         }
     private:
         long p;
         long q;
         long M;
-        long rand;
+        long my_rand;
 };
 
 int main() {
     //assuming valid params now
-    BBS bbs((long)11, (long)23);
+    BBS bbs((long)211, (long)107);
     // srand(time(NULL));
-    long seed = rand();
-    while (bbs.set_seed(seed) != 1) {
-        seed = rand();
-    }
+    // long seed = rand();
+    // while (bbs.set_seed(seed) != 1) {
+    //     seed = rand();
+    // }
+    long seed = 151;
+    bbs.set_seed(seed);
     printf("seed is %ld \n", seed);
-    for (int i = 0; i < 10; i++) {
-        printf("%ld \n", bbs.get_rand());
+    for (int i = 0; i < 350; i++) {
+        printf("iter: %d %ld \n", i, bbs.get_rand());
     }
 }
 
